7fc5883d825073d954feaa59a9bd8f7f83d96c31
Yirong Qin <yq4103@mun.ca>
2021-11-17 18:25:05 -0500

    number guessing game

---
+++ numberGuesser.js
@@ -0,0 +1,30 @@
+let prompt = require("prompt-sync")();
+
+let randomNum = Math.floor(Math.random() * 101);
+
+let answer = prompt("Guess a number, choose from 0 to 100: ");
+
+const guess = function() {
+  
+  let attempts = 0;
+
+  do {
+    attempts++;
+    if (!Number.isInteger(parseInt(answer)) || parseInt(answer) < 0 || parseInt(answer) > 100) {
+      console.log("Please enter an interger from 0 to 100.");
+      answer = prompt("Guess a number, choose from 0 to 100: ");
+    } else if (parseInt(answer) < randomNum) {
+      console.log("Guess higher.");
+      answer = prompt("Guess a number, choose from 0 to 100: ");
+    } else if (parseInt(answer) > randomNum) {
+      console.log("Guess lower.");
+      answer = prompt("Guess a number, choose from 0 to 100: ");
+    } else if (parseInt(answer) === randomNum) {
+      console.log(`You got it in ${attempts} attempts.`);
+      break;
+    }
+  } while (answer !== randomNum);
+};
+
+guess();
+
