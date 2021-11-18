e7a8c2ed05d0b7b29a637c79e13149ba370938ec
Yirong Qin <yq4103@mun.ca>
2021-11-17 19:49:35 -0500

    guess an integer from 0 to 100

---
+++ README.md
@@ -0,0 +1,5 @@
+A guessing game where the user has to guess a secret integer from 0 to 100. After every guess the program tells the user whether their integer was too large or too small.
+
+If the user provides an answer which isn't an integer, or an integer above 100 or below 0, it does not count as an attempt.
+
+When the user suceed, the number of attempts will be printed.
--- numberGuesser.js
+++ numberGuesser.js
@@ -7,19 +7,27 @@ let answer = prompt("Guess a number, choose from 0 to 100: ");
 const guess = function() {
   
   let attempts = 0;
+  let previousAnswers = [];
 
   do {
-    attempts++;
-    if (!Number.isInteger(parseInt(answer)) || parseInt(answer) < 0 || parseInt(answer) > 100) {
+    if (!Number.isInteger(Number(answer)) || parseInt(answer) < 0 || parseInt(answer) > 100) {
       console.log("Please enter an interger from 0 to 100.");
       answer = prompt("Guess a number, choose from 0 to 100: ");
+    } else if (previousAnswers.includes(answer)) {
+      console.log("You have already guessed this number.");
+      answer = prompt("Guess a number, choose from 0 to 100: ");
     } else if (parseInt(answer) < randomNum) {
+      attempts++;
+      previousAnswers.push(answer);
       console.log("Guess higher.");
       answer = prompt("Guess a number, choose from 0 to 100: ");
     } else if (parseInt(answer) > randomNum) {
+      attempts++;
+      previousAnswers.push(answer);
       console.log("Guess lower.");
       answer = prompt("Guess a number, choose from 0 to 100: ");
     } else if (parseInt(answer) === randomNum) {
+      attempts++;
       console.log(`You got it in ${attempts} attempts.`);
       break;
     }
